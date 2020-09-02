5ef69427edbbd35c0ffdfdd2797a7aa028160f60
Toms Krauklis <tomskrauklis@Tomss-MBP.cust.communityfibre.co.uk>
2020-09-02 07:28:42 +0100

    Updates

---
+++ precache-manifest.b8aba2a806edd70df2df015790e7708c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8b4427cd900815bf90a53b076f7f5b",
+    "url": "/cats-rolodex/index.html"
+  },
+  {
+    "revision": "3383d96e95be278e52e1",
+    "url": "/cats-rolodex/static/css/main.e74d0789.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/cats-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cats-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3383d96e95be278e52e1",
+    "url": "/cats-rolodex/static/js/main.91fb298c.chunk.js"
+  },
+  {
+    "revision": "c233e02397e509ffb3f4",
+    "url": "/cats-rolodex/static/js/runtime-main.474d8514.js"
+  }
+]);
